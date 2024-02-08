502474b3f8b272756f72a289f887f88bedabcaf4
Mohamedmoussa-m <mohamedmoussa97@hotmail.com>
2024-02-08 07:56:45 +0100

    fixed preprocessed img // added notifie // added compare // fixed some bugs

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from pynput import keyboard
 import keyboard
 import time
 import pytesseract
+import pygame
+import cv2
+import numpy as np
+import re
 
 
 '''
@@ -25,14 +29,14 @@ check_item(item):
 
 '''
 # Variables
-items = ["solomonk"]
+items = ["chapeau de la sain ballotin"]
 
 
 # CONSTANTS
 search_bar = (230, 197)
 first_item_found = (85, 379, 323, 27)
-first_price_reg = (820, 388, 200, 48)
-second_price_reg = (819, 465, 203, 50)
+first_price_reg = (852, 402, 139, 26)
+second_price_reg = (855, 476, 134, 26)
 item_found_reg = (41, 366, 81, 50)
 
 # ASSETS
@@ -41,8 +45,8 @@ item_found_reg = (41, 366, 81, 50)
 
 def search(item):
     pyautogui.click(search_bar, clicks=2, interval=.5)
-    time.sleep(.3)
-    keyboard.write(item)
+    time.sleep(.5)
+    keyboard.write(item, delay=.1)
     keyboard.press_and_release('enter')
 
 
@@ -58,17 +62,78 @@ def wait_item():
 def open_item():
     item_found = wait_item()
     if item_found:
-        time.sleep(.1)
+        time.sleep(.4)
         pyautogui.click(first_item_found)
 
+        
+def preprocess_image(image):
+    # Convert image to grayscale
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    
+    # Invert colors
+    inverted = cv2.bitwise_not(gray)
+    
+    # Apply adaptive thresholding
+    threshold = cv2.adaptiveThreshold(inverted, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+    
+    return threshold
+
 def ocr_this(reg):
+    # Capture screenshot
     roi = pyautogui.screenshot(region=reg)
-    custom_config = r'--oem 3 --psm 3'
-    text = pytesseract.image_to_string(roi, config=custom_config)
-    if len(text) == 0:
-        return False
-    # print(text)   #test
+    
+    # Convert screenshot to OpenCV format
+    image = np.array(roi)
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    
+    # Preprocess image
+    preprocessed_image = preprocess_image(image)
+    
+    # Perform OCR on preprocessed image
+    custom_config = r'--oem 3 --psm 4'
+    text = pytesseract.image_to_string(preprocessed_image, config=custom_config)
+
+    text = re.sub(r'[\s.]', '', text)
     return text
+        
+
+
+def compare_first_seconde():
+    time.sleep(.5)
+    try:
+        first_price = ocr_this(first_price_reg)
+        second_price = ocr_this(second_price_reg)
+        first_price = int(first_price)
+        second_price = int(second_price)
+    except:
+        print("MA BGHACH HADCHI I WLI INT, SBAR")
+        time.sleep(1)
+        try: 
+            first_price = ocr_this(first_price_reg)
+            second_price = ocr_this(second_price_reg)
+            first_price = int(first_price)
+            second_price = int(second_price)
+        except:
+            print("SIR T9AWED")
+
+    taux = first_price / second_price
+    if taux <= 1:
+        notif_sound()
+
+def notif_sound():
+    pygame.mixer.init()
+    wav = "assets/mixkit-confirmation-tone-2867.wav"
+    sound = pygame.mixer.Sound(wav)
+    sound.play()
+    while pygame.mixer.get_busy():
+        pygame.time.wait(100)
+    
+
+
+    
+
+    
+    
 
 
 
@@ -77,9 +142,11 @@ def ocr_this(reg):
 
 
 if __name__ == "__main__":
-    # search(items[0])
-    # wait_item()
-
+    search(items[0])
+    wait_item()
+    open_item()
+    compare_first_seconde()
+    
     pass
 
 
